44b1f50e08ba50979d1eb67f350f886b76505332
YoruAkio <yoruakio@proton.me>
2026-01-14 22:40:45 +0700

    feat: adding favicon; adding logo on docs sidebar title

--- app/layout.config.tsx
+++ app/layout.config.tsx
@@ -1,8 +1,19 @@
 import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
+import Image from 'next/image';
 
 export const baseOptions: BaseLayoutProps = {
   nav: {
-    title: 'Gurotopia',
+    title: (
+      <div className="flex items-center gap-2">
+        <Image 
+          src="https://raw.githubusercontent.com/YoruAkio/ProjectAssets/refs/heads/main/akio/guro/logo.png" 
+          width={32} 
+          height={32} 
+          alt="Gurotopia" 
+        />
+        <span>Gurotopia</span>
+      </div>
+    ),
   },
   links: [],
   githubUrl: 'https://github.com/gurotopia',
--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,16 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com',
+        port: '',
+        pathname: '/**',
+      }
+    ]
+  }
 };
 
 const withMDX = createMDX();
